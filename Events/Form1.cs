c374eebb5dcb2eb0ac1527e1fabc5857bb348f2f
Fatima <fatimavaliyeva0704@gmail.com>
2024-09-03 18:23:12 +0400

    first commit

---
+++ Events/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events
+{
+    public partial class Form1 : Form
+    {
+        private MainAccount _mainAccount;
+        private SavingAccount _secondAccount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _mainAccount = new MainAccount(100);
+            _secondAccount = new SavingAccount(400);
+            _mainAccount.SavingAccount = _secondAccount;
+
+            _mainAccount.Deposit(100);
+            _secondAccount.Deposit(200);
+            txtMainAccountBalance.Text = _mainAccount.GetBalance().ToString("C");
+            txtSecondAccountBalance.Text = _secondAccount.GetBalance().ToString("C");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            MessageBox.Show("Hello from method");
+        }
+
+        private void btnMainAccountDeposit_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtMainAccountDepositValue.Text, out decimal value))
+            {
+                _mainAccount.Deposit(value);
+                txtMainAccountBalance.Text = _mainAccount.GetBalance().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Duzgun formatda eded daxil edilmeyib");
+            }
+        }
+
+        private void btnSecondAccountDeposit_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtSecondAccountDepositValue.Text, out decimal value))
+            {
+                _secondAccount.Deposit(value);
+                txtSecondAccountBalance.Text = _secondAccount.GetBalance().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Duzgun formatda eded daxil edilmeyib");
+            }
+        }
+
+        private void btnMainAccountWithdraw_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtMainAccountWithdrawValue.Text, out decimal value))
+            {
+                _mainAccount.Withdraw(value);
+                txtMainAccountBalance.Text = _mainAccount.GetBalance().ToString();
+                txtSecondAccountBalance.Text = _secondAccount.GetBalance().ToString();
+            }
+        }
+
+        //private void btnSecondAccountWithdraw_Click(object sender, EventArgs e)
+        //{
+        //    if (decimal.TryParse(txtSecondAccountWithdrawValue.Text, out decimal value))
+        //    {
+        //        _secondAccount.Withdraw(value);
+        //        txtSecondAccountBalance.Text = _secondAccount.GetBalance().ToString();
+        //    }
+        //}
+
+        private void btnSecondAccountWithdraw_Click_1(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtSecondAccountWithdrawValue.Text, out decimal value))
+            {
+                _secondAccount.Withdraw(value);
+                txtSecondAccountBalance.Text = _secondAccount.GetBalance().ToString();
+            }
+        }
+    }
+}
---
+++ Events/MainAccount.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events
+{
+    internal class MainAccount : BankAccount
+    {
+        public BankAccount SavingAccount { get; set; }
+
+        public MainAccount(decimal balance) : base(balance)
+        {
+
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (Balance + SavingAccount.Balance >= amount)
+            {
+                if (amount >= Balance)
+                {
+                    decimal sub = amount - Balance;
+                    Balance = 0;
+                    SavingAccount.Balance -= sub;
+                }
+                else
+                {
+                    Balance -= amount;
+                }
+            }
+            else
+            {
+               OnOverdrawnEventHandler(amount - (Balance + SavingAccount.Balance));
+            }
+        }
+    }
+
+    internal class SavingAccount : BankAccount
+    {
+        public SavingAccount(decimal balance) : base(balance)
+        {
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                //OnOverdrawnEventHandler(amount);
+                MessageBox.Show("Bomj vaxtlardi");
+            }
+        }
+        
+        public override void OnOverdrawnEventHandler(decimal subAmount)
+        {
+            base.OnOverdrawnEventHandler(subAmount);
+            //OverdrawnEventHandler.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    internal class BankAccount
+    {
+        public EventHandler OverdrawnEventHandler;
+
+        public decimal Balance { get; set; }
+
+        public BankAccount(decimal balance)
+        {
+            Balance = balance;
+            OverdrawnEventHandler = new EventHandler(OverdrawnEventMethod);      
+        }   
+
+        public virtual void OnOverdrawnEventHandler(decimal subAmount)
+        {
+            if (OverdrawnEventHandler != null)
+                OverdrawnEventHandler.Invoke(this, new OverdrawnEventArgs(subAmount));
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void OverdrawnEventMethod(object sender, EventArgs e)
+        {
+            OverdrawnEventArgs args = (OverdrawnEventArgs)e;
+            MainAccount bankAccount = (MainAccount)sender;
+
+            MessageBox.Show("Balansinda pul yoxdur " + args.SubAmount + " " + (bankAccount.Balance + bankAccount.SavingAccount.Balance));
+        }
+
+        public virtual void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                decimal shortfall = amount - Balance;
+                OnOverdrawnEventHandler(shortfall); 
+            }
+        }
+
+        public decimal GetBalance() => Balance;
+    }
+
+    internal class OverdrawnEventArgs : EventArgs
+    {
+        public OverdrawnEventArgs(decimal subAmount)
+        {
+            SubAmount = subAmount;
+        }
+
+        public decimal SubAmount { get; set; }       
+    }
+}
