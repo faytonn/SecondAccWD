123eb5290508beb4dc170aa0a3a310cb1b039339
Fatima <fatimavaliyeva0704@gmail.com>
2024-09-05 18:59:09 +0400

    .

--- Events/MainAccount.cs
+++ Events/MainAccount.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Events
@@ -53,7 +48,7 @@ namespace Events
             }
             else
             {
-                OnOverdrawnEventHandler(amount);
+                OnOverdrawnEventHandler(amount - Balance);
                 //MessageBox.Show("Bomj vaxtlardi");
             }
         }
